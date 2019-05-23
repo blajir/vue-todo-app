f81967f22531db20e446b9348be747d74af14535
blajir <hata.junichi@gmail.com>
2019-05-23 23:46:46 +0900

    create todo-complete-list

---
+++ src/views/CompleteList.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .wrapper
+    h1
+      |complete
+
+    ul
+      todo-list-item(
+        v-for="(item, index) in completeTodoItems"
+        :key="item.id"
+        :todoItem="item"
+        :itemIndex="index"
+        @removeTodoItem="removeTodo"
+      )
+
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+import * as types from '@/mutation-types'
+
+import TodoListItem from '@/components/TodoListItem'
+
+const components = {
+  TodoListItem
+}
+
+export default {
+  components,
+  computed: {
+    ...mapGetters([
+      'completeTodoItems'
+    ])
+  },
+  methods: {
+    addTodo(message) {
+      this.$store.commit(types.ADD_TODO, message)
+    },
+    removeTodo(index) {
+      this.$store.commit(types.REMOEVE_TODO, index)
+    }
+  }
+}
+</script>
