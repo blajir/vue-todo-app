614a6456be4cda72a46a3bd668bc6881a3cd86ed
blajir <hata.junichi@gmail.com>
2019-05-25 11:22:48 +0900

    create Navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  nav
+    ul
+      li
+        router-link(to="/")
+          |all
+      li
+        router-link(to="/complete")
+          |complete
+</template>
+
+<script>
+export default {
+  name: 'todoNavigation'
+}
+</script>
