f098d6a8056fabdfcece9b094fa4129276e157c4
blajir <hata.junichi@gmail.com>
2019-05-26 23:38:29 +0900

    add vue.config.js for sourcemap

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-  nav
-    ul
-      li
+  nav#nav
+    ul.navlist
+      li.navlist__item
         router-link(to="/")
           |all
-      li
+      li.navlist__item
         router-link(to="/complete")
           |complete
 </template>
@@ -14,3 +14,20 @@ export default {
   name: 'todoNavigation'
 }
 </script>
+
+<style lang="scss" scoped>
+#nav {
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.navlist {}
+.navlist__item {
+  list-style: none;
+  text-align: left;
+}
+</style>
